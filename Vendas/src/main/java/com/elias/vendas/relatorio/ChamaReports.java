ffa1cd8a529c4df07cb6bf288cfce7338dc64403
EliasBarret <elias-barreto@outlook.com>
2019-04-23 23:42:56 -0300

    Ajustes nas telas e nos relatórios

--- Vendas/src/main/java/com/elias/vendas/relatorio/ChamaReports.java
+++ Vendas/src/main/java/com/elias/vendas/relatorio/ChamaReports.java
@@ -1,13 +1,20 @@
 package com.elias.vendas.relatorio;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.imageio.stream.FileImageInputStream;
+import javax.swing.ImageIcon;
 
 import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
 
 import com.elias.vendas.util.HibernateUtil;
+import com.itextpdf.text.pdf.codec.Base64.InputStream;
 
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -19,10 +26,13 @@ public class ChamaReports {
 	public void relatorioVenda() {
 		try {
 
+			Map<String, Object> parametros = new HashMap<>();
+			//Image logo = new ImageIcon(getClass().getResource("/resources/images/sale.png")).getImage();
 			String caminho = Faces.getRealPath("/reports/r_geral_vendas.jasper");
 
-			Map<String, Object> parametros = new HashMap<>();
+
 			parametros.put("CODVENDA", 1);
+			//parametros.put("LOGO", caminhoImagemBrasao);
 
 			Connection conexao = HibernateUtil.getConnection();
 
@@ -42,9 +52,11 @@ public class ChamaReports {
 
 			// Users/elias/eclipse-workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Vendas/reports/r_comp_venda.jasper
 			String caminho = Faces.getRealPath("/reports/r_comp_venda.jasper");
-
+		//	Image logo = new ImageIcon(getClass().getResource("/resources/images/sale.png")).getImage();
+			
 			Map<String, Object> parametros = new HashMap<>();
 			parametros.put("CODVENDA", id);
+		//	parametros.put("LOGO", logo);
 
 			Connection conexao = HibernateUtil.getConnection();
 
@@ -65,9 +77,11 @@ public class ChamaReports {
 
 			// Users/elias/eclipse-workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Vendas/reports/r_comp_venda.jasper
 			String caminho = Faces.getRealPath("/reports/r_geral_tipo_venda.jasper");
-
+		//	Image logo = new ImageIcon(getClass().getResource("/resources/images/sale.png")).getImage();
+			
 			Map<String, Object> parametros = new HashMap<>();
 			parametros.put("TIPOVENDA", tipo);
+			//parametros.put("LOGO", logo);
 
 			Connection conexao = HibernateUtil.getConnection();
 
